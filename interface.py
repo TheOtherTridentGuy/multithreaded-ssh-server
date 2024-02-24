86e4e2ef62fdd0786d9be6b6f5994589aafb879e
TheOtherTridentGuy <146895895+TheOtherTridentGuy@users.noreply.github.com>
2024-02-24 03:20:58 +0000

    Get it working!

---
+++ interface.py
@@ -0,0 +1,49 @@
+import paramiko
+import socket  
+import threading  
+
+class Server(paramiko.server.ServerInterface): 
+    def get_allowed_auths(self, username): 
+        return "password"
+    
+    def check_channel_request(self, kind, channelID):
+        return paramiko.OPEN_SUCCEEDED
+    
+    def check_channel_shell_request(self, channel):
+        return True
+    
+    def check_channel_pty_request(self, c, t, w, h, p, ph, m):
+        return True
+    
+    def get_banner(self):
+        return ("Paramiko SSH Server v1.0\n\r", "EN")
+
+    def check_auth_password(self, username, password):
+        print(f"[*] Auth request with credentials {username}:{password}")
+        return paramiko.AUTH_SUCCESSFUL
+
+def handle(conn, addr):
+    print(f"[*] Connection from {addr[0]}:{addr[1]}, waiting for SSH connection...")
+    transport = paramiko.Transport(conn)
+    transport.add_server_key(host_key)
+    transport.start_server(server=server)
+    channel = transport.accept(30) 
+    if channel:
+        print("[*] SSH connection recieved")
+        channel.send("Hi :)\r\n")
+        print(f"[>] {channel.recv(1024)}")
+        channel.close()
+
+
+host_key = paramiko.RSAKey.generate(2048)
+server = Server()
+sock = socket.socket()
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(("127.0.0.1", 5555))
+sock.listen(100)
+print("[*] Socket listening")
+while True:
+    conn, addr = sock.accept()
+    handler = threading.Thread(target=handle, args=(conn, addr))
+    handler.run()
+    
